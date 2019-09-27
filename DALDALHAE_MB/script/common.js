52bea491a737d0369c94f176018c45ed9d3e7890
HyunjiZzang <lake0989@naver.com>
2019-09-27 15:19:03 +0900

    dal_mb
    mobile

--- DALDALHAE_MB/script/common.js
+++ DALDALHAE_MB/script/common.js
@@ -7,13 +7,4 @@ $(document).ready(function (){
         else $('#subnav').removeClass('scolled');
 	});
 
-	/* TOP 버튼 */
-	$(window).on('scroll', function() {
-		var scrollT = $(this).scrollTop();
-		console.log(scrollT);
-
-		if (scrollT > 90) $('#subnav').addClass('scolled');
-        else $('#subnav').removeClass('scolled');
-	});
-
 });
